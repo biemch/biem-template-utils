c839b83328844afa70de782c6c10216c6d5399e1
Michael Freund <michael@biem.ch>
2024-12-28 08:10:06 +0100

    fix: remove unused export of vite template plugin from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { useTemplateValue } from './hook/useTemplateValue.js';
 
-export { vitePluginBiem } from './plugin/vite/template.js';
-
 export type { Config } from './type/config.js';
 export type { FieldList } from './type/field-list.js';
 export type { Template } from './type/template.js';
