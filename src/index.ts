251cbf286b24dc0855c1d752bf214bb177ed3a1a
Michael Freund <michael@biem.ch>
2024-12-28 09:46:37 +0100

    fix: remove unnecessary blank lines in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { useTemplateValue } from './hook/useTemplateValue.js';
-
 export type { Config } from './type/config.js';
 export type { FieldList } from './type/field-list.js';
 export type { Template } from './type/template.js';
-
 export { replacePlaceholders } from './util/template.js';
