31248364537e7804aa9f9834fd5cca27630b9403
Michael Freund <michael@biem.ch>
2024-12-28 08:08:16 +0100

    fix: update eslint config to ignore dist directory

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+	{ ignores: ['dist/'] },
 	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
 	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 	pluginJs.configs.recommended,
