f26b76fac603526f43f3688d03b2512e9530e4ec
Michael Freund <michael@biem.ch>
2024-12-30 13:40:59 +0100

    chore: update @biem/eslint-config dependency

--- eslint.config.js
+++ eslint.config.js
@@ -1,53 +1,6 @@
-import pluginJs from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import importNewlines from 'eslint-plugin-import-newlines';
-import pluginReact from 'eslint-plugin-react';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import unusedImports from 'eslint-plugin-unused-imports';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import config from '@biem/eslint-config';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{
-		plugins: {
-			'simple-import-sort': simpleImportSort,
-			'unused-imports': unusedImports,
-			'import-newlines': importNewlines,
-		},
-	},
-	{ ignores: ['dist/'] },
-	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	pluginReact.configs.flat.recommended,
-	stylistic.configs.customize({
-		indent: 'tab',
-		quotes: 'single',
-		semi: true,
-		jsx: true,
-	}),
-	{
-		settings: {
-			react: {
-				version: 'detect',
-			},
-		},
-	},
-	{
-		rules: {
-			'no-explicit-any': 0,
-			'jsx-quotes': [2, 'prefer-double'],
-			'import-newlines/enforce': ['error', { 'items': 1, 'max-len': 160, 'semi': true }],
-			'object-curly-spacing': ['error', 'always'],
-			'@stylistic/indent': ['error', 'tab'],
-			'@stylistic/no-mixed-spaces-and-tabs': 'error',
-			'@stylistic/object-curly-spacing': ['error', 'always'],
-			'@typescript-eslint/no-explicit-any': 'off',
-			'simple-import-sort/imports': 'error',
-			'simple-import-sort/exports': 'error',
-			'unused-imports/no-unused-imports': 'error',
-		},
-	},
+	...config,
 ];
